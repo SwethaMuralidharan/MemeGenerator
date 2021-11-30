324127b896a03c8f1b4cb7ba18ab3a198ee0e566
Swetha <swetha.blue.sky@gmail.com>
2021-11-29 21:49:40 -0800

    created meme-form component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+function Header(){
+    return(
+        <header>
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"/>
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+export default Header
